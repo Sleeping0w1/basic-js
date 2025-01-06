be3deab4d4f196236d946637b60fdc973898cd63
Sleeping0w1 <1033351@gmail.com>
2025-01-06 22:14:57 +0300

    feat: solution for minesweeper

--- src/mine-sweeper.js
+++ src/mine-sweeper.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * In the popular Minesweeper game you have a board with some mines and those cells
@@ -23,11 +23,25 @@ const { NotImplementedError } = require('../extensions/index.js');
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function minesweeper(matrix) {
+  const field = [];
+  for (let i = 0; i < matrix.length; i++) {
+    field[i] = [];
+    for (let j = 0; j < matrix[i].length; j++) {
+      field[i][j] =
+        (i + 1 < matrix.length ? +matrix[i + 1][j] : 0) +
+        (j + 1 < matrix.length ? +matrix[i][j + 1] : 0) +
+        (i - 1 >= 0 ? +matrix[i - 1][j] : 0) +
+        (j - 1 >= 0 ? +matrix[i][j - 1] : 0) +
+        ((i + 1 < matrix.length) && (j + 1 < matrix.length) ? +matrix[i + 1][j + 1] : 0) +
+        ((i - 1 >= 0) && (j - 1 >= 0)  ? +matrix[i - 1][j - 1] : 0) +
+        ((i + 1 < matrix.length) && (j - 1 >= 0)  ? +matrix[i + 1][j - 1] : 0) +
+        ((i - 1 >= 0) && (j + 1 < matrix.length)  ? +matrix[i - 1][j + 1] : 0);
+    }
+  }
+  return field;
 }
 
 module.exports = {
-  minesweeper
+  minesweeper,
 };
